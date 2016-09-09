94e2cd92c318763affe883193b2652069543699d
Max <agnusmaximus@berkeley.edu>
2016-09-08 17:12:20 -0700

    Add license and authors

--- src/Model/IsingGibbsModel.h
+++ src/Model/IsingGibbsModel.h
@@ -1,3 +1,19 @@
+
+/*
+* Copyright 2016 [See AUTHORS file for list of authors]
+*
+*    Licensed under the Apache License, Version 2.0 (the "License");
+*    you may not use this file except in compliance with the License.
+*    You may obtain a copy of the License at
+*
+*        http://www.apache.org/licenses/LICENSE-2.0
+*
+*    Unless required by applicable law or agreed to in writing, software
+*    distributed under the License is distributed on an "AS IS" BASIS,
+*    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*    See the License for the specific language governing permissions and
+*    limitations under the License.
+*/
 // To compare against file:
 // ./cyclades --interval_print=20000 --print_loss_per_epoch --should_compare_to_compare_distribution_file=true  --should_write_to_distribution_output_file=false  --cyclades_batch_size=200  -ising_gibbs -n_threads=16 --random_per_batch_datapoint_processing  -hogwild_trainer  -print_partition_time -n_epochs=200000 -sgd --data_file="data/gibbs/gibbs.data"
 //
