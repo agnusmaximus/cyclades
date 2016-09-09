d5bfdddbb781219b5ca5a394dcb9dd17aeea5d3f
Max <agnusmaximus@berkeley.edu>
2016-09-08 19:05:47 -0700

    Update

--- data/fetch_all_data.sh
+++ data/fetch_all_data.sh
@@ -2,7 +2,7 @@ for dir in ./*/
 do
     echo "Fetching data for directory: " $dir
     cd $dir
-    if [ ! -f get_data.sh ]; then
+    if [ -f get_data.sh ]; then
 	sh get_data.sh 2>&1 > /dev/null
     fi
     cd ..
