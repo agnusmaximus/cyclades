d376662ca2ec1e2778d95c0ca81dd0d00f6a85b8
Max Lam <agnusmaximus@mayhem.Millennium.Berkeley.EDU>
2016-09-08 18:52:08 -0700

    Update

--- data/word_embeddings/get_data.sh
+++ data/word_embeddings/get_data.sh
@@ -1,3 +1,3 @@
-wget -qO- http://www.mattmahoney.net/dc/enwik8.zip | tar -xvf-
+wget -qO- http://www.mattmahoney.net/dc/enwik8.zip | tar xvz
 perl wikifil.pl enwik8 > cleaned_corpus
 python create_graph_from_corpus.py cleaned_corpus
