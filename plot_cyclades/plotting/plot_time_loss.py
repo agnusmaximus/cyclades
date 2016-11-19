cde686a8e4a652ca5327d1db709b7d777fcd3785
Max <agnusmaximus@berkeley.edu>
2016-11-19 05:44:25 -0800

    Add plot code

---
+++ plot_cyclades/plotting/plot_time_loss.py
@@ -0,0 +1,138 @@
+# sample call: python plotting/plot_time_loss.py plotting/plot_time_loss.setting "Matrix Completion 8 threads - Movielens 1m" 0
+
+from __future__ import print_function
+import sys
+
+import matplotlib
+matplotlib.use('Agg')
+
+import matplotlib.ticker as ticker
+import matplotlib.lines as mlines
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+import re
+import subprocess
+from matplotlib.ticker import FormatStrFormatter
+import pylab
+import commands
+import numpy as np
+import os
+
+# Plot customization
+f, ax = plt.subplots(figsize=(15,15))
+ax.set_xlabel("Time/s", fontsize=26)
+ttl = ax.title
+ttl.set_position([.5, 1.05])
+ax.set_ylabel("Objective Value", fontsize=26)
+ax.set_yscale('log')
+# ax.set_xscale('log') # Make cyclades cache behavior look impressive.
+ax.tick_params(axis='both', which='major', labelsize=26, pad=7)
+ax.tick_params(axis='both', which='minor', labelsize=26, pad=7)
+
+partition_time_matcher = re.compile("Partition Time\(s\): (.*)")
+plot_cache_directory = "./plotting_cache/"
+
+def RunCommand(command, use_cached_output, reps=3):
+    out = ""
+    command_file = plot_cache_directory + "".join(x for x in command if x.isalnum())
+    if not use_cached_output:
+        # We expect the output of the command to be of form:
+        # Line 1: Partition Time(s): ...
+        # Lines 2+: Epoch: %d\tTime: %f\tLoss:%f
+        print("Running command: %s" % command)
+        for rep in range(reps):
+            out += commands.getstatusoutput(command)[1] + "\n"
+        f_cache_out = open(command_file, "w+")
+        print(out.strip(), file=f_cache_out);
+        f_cache_out.close()
+    else:
+        print("Loading command file: %s" % command_file)
+        f_cache_in = open(command_file, "r")
+        out = f_cache_in.read().strip()
+        f_cache_in.close()
+
+    lines = out.split("\n")
+    partition_time = 0
+    epoch_time_data, epoch_loss_data = {}, {}
+    n_epochs = 0
+    for line in lines:
+        if line.strip() == "":
+            continue
+        partitioning_match = partition_time_matcher.search(line)
+        if partitioning_match:
+            partition_time = float(partitioning_match.group(1))
+        else:
+            epoch, time, loss = [x for x in line.split("\t") if x != ""]
+            epoch = int(epoch.split(":")[-1].strip())
+            time = float(time.split(":")[-1].strip())
+            loss = float(loss.split(":")[-1].strip())
+            if epoch not in epoch_time_data:
+                epoch_time_data[epoch] = 0
+            if epoch not in epoch_loss_data:
+                epoch_loss_data[epoch] = 0
+            epoch_time_data[epoch] += time + partition_time
+            epoch_loss_data[epoch] += loss
+            n_epochs = max(n_epochs, epoch)
+    for epoch, value in epoch_time_data.items():
+        epoch_time_data[epoch] = value / reps
+    for epoch, value in epoch_loss_data.items():
+        epoch_loss_data[epoch] = value / reps
+    times = [epoch_time_data[x] for x in range(n_epochs)]
+    losses = [epoch_loss_data[x] for x in range(n_epochs)]
+    return times, losses
+
+def GetSettings(fname):
+    # We expect the following format:
+    # "command" curve_title curve_color
+    f = open(fname)
+    settings = []
+    for line in f:
+        elements = [x.strip() for x in line.split(",")]
+        setting = {"command" : "", "plot_title" : "", "plot_color" : ""}
+        setting["command"] = elements[0]
+        setting["plot_title"] = elements[1] # Curve title
+        setting["plot_color"] = elements[2] # Curve color
+        setting["line_style"] = elements[3]
+        settings.append(setting)
+    return settings
+
+def Plot(name, settings, use_cached_output):
+    minx,maxx,miny,maxy = float("inf"),float("-inf"),float("inf"),float("-inf"),
+    for setting in settings:
+        # Run command and plot
+        command = setting["command"]
+        times, losses = RunCommand(command, use_cached_output)
+        ax.plot(times, losses, color=setting["plot_color"], linewidth=8, label=setting["plot_title"], linestyle=setting["line_style"])
+
+        # Bounds tuning.
+        minx=min(minx,min(times))
+        maxx=max(maxx,max(times))
+        miny=min(miny,min(losses), 100**int(np.log10(min(losses))))
+        maxy=max(maxy,max(losses))
+
+    # scale
+    maxy = min(maxy, 10**20)
+    ax.axis([minx*.95,maxx*1.05,miny*.95,maxy*1.05])
+
+    # Do some stuff to make things nice looking.
+    larger_power_10 = 10 ** int(np.log10(miny*.95))+1
+    if larger_power_10 > maxy*1.05:
+        plt.tick_params(axis='y', which='minor')
+        ax.yaxis.set_minor_formatter(FormatStrFormatter("%.1f"))
+
+    basex = basey = 100
+    #ax.xaxis.set_major_locator(ticker.LogLocator(base = basex))
+    ax.yaxis.set_major_locator(ticker.LogLocator(base = basey))
+    ax.legend(loc="upper right")
+    plt.savefig(name, bbox_inches='tight')
+
+if __name__=="__main__":
+    if len(sys.argv) != 4:
+        print("Usage: plot_time_loss.py settings_file output_name use_cached_output")
+        sys.exit(0)
+
+    if not os.path.exists(plot_cache_directory):
+        os.makedirs(plot_cache_directory)
+    ax.set_title(sys.argv[2], fontsize=26)
+    settings = GetSettings(sys.argv[1])
+    Plot(sys.argv[2], settings, int(sys.argv[3]))
