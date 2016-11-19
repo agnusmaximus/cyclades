dfa4a94f6ebae087d8e31428fa823fa778b83763
Max <agnusmaximus@berkeley.edu>
2016-11-19 07:52:07 -0800

    svrg

--- plotting/plot_github_figures.sh
+++ plotting/plot_github_figures.sh
@@ -1,3 +1,3 @@
-python ./plotting/plot_time_loss.py plotting/nh2010.settings "SAGA Least Squares 2 threads - NH2010" 0
-python ./plotting/plot_speedup.py plotting/nh2010_speedup.settings "Least Squares Speedup - NH2010" 0
+python ./plotting/plot_time_loss.py plotting/graph_eigen_nh2010.settings "SAGA Least Squares 2 threads - NH2010" 0
+python ./plotting/plot_speedup.py plotting/graph_eigen_nh2010_speedup.settings "Least Squares Speedup - NH2010" 0
 python ./plotting/plot_speedup.py plotting/ml-10m_speedup.setting "Matrix Completion Speedup" 0
